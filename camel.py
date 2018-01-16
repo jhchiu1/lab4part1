94daee24f95e9dcd2266cd8ef0730b2e1d654380
juliechiu <jhchiu1@gmail.com>
2018-01-16 13:44:19 -0600

    add branch called instructions

--- camel.py
+++ camel.py
@@ -1,5 +1,12 @@
 # Camel case
 
+'''Display program name in a banner'''
+msg = 'AWESOME camelCaseGenerator PROGRAM'
+stars = '*' * len(msg)
+print('\n', stars, '\n', msg, '\n', stars, '\n')
+
+
+
 # Create sentence variable asking for user input
 sentence = input("Enter a sentence:\n")
 
@@ -9,11 +16,12 @@ pastFirstWord = False
 
 # Use split method to break up the string
 for word in sentence.split():
-    if pastFirstWord:
-        newSentence += word.capitalize()
-    else:
-        newSentence += word.lower()
-    pastFirstWord = True
+	   if pastFirstWord:
+	       newSentence += word.capitalize()
+	   else:
+	       newSentence += word.lower()
+	   pastFirstWord = True
 
 # Print newly converted sentence
 print(newSentence)
+
