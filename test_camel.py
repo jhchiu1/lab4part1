167891f08c0559f7158b4844907d1380e47df7ac
Julie <jhchiu1@gmail.com>
2018-03-14 20:39:59 -0500

    Added number and symbol tests

--- test_camel.py
+++ test_camel.py
@@ -12,6 +12,12 @@ class TestCamelCase(unittest.TestCase):
 
 	def test_camel_case_spaces(self):
 		self.assertEqual('helloWorld', camel.camel_case('	Hello   World   '))
+		
+	def test_numbers(self):
+        	self.assertEqual('123', camel.camel_case('123'))
+
+	def test_symbols(self):
+    		self.assertEqual('@@#^^', camelcase.camel_case('@@# ^ ^'))
 
 if __name__ == '__main__':
     unittest.main()
